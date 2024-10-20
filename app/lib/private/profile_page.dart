2f1ecb7e8cb243358dd6206d04d6e61fa8ade477
holmes301 <quintonrholmes30@gmail.com>
2024-10-19 20:33:57 -0400

    added padding

--- app/lib/private/profile_page.dart
+++ app/lib/private/profile_page.dart
@@ -74,164 +74,168 @@ class ProfilePageState extends State<ProfilePage> {
         body: CustomScrollView(
           slivers: <Widget>[
             SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'My Account',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  ClipOval(
-                    child: Image.asset(
-                      'assets/IMG_7421.png', // Assuming the image is stored in the assets folder
-                      width: 80, // Adjust the width as needed
-                      height: 80, // Adjust the height as needed
+              child: Padding(
+                padding: EdgeInsetsDirectional.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'My Account',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Welcome, brutus_buckeye!',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 20),
-                  Visibility(
-                    visible: !_isVisible,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'My Info:',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 20),
-                        TextFormField(
-                          controller: _fullNameController,
-                          decoration: InputDecoration(
-                            labelText: 'Full Name',
-                            border: OutlineInputBorder(),
+                    SizedBox(height: 10),
+                    ClipOval(
+                      child: Image.asset(
+                        'assets/IMG_7421.png', // Assuming the image is stored in the assets folder
+                        width: 80, // Adjust the width as needed
+                        height: 80, // Adjust the height as needed
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Welcome, brutus_buckeye!',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    Visibility(
+                      visible: !_isVisible,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'My Info:',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        ListTile(
-                          title: const Text('Male'),
-                          leading: Radio<String>(
-                            value: 'Male',
-                            groupValue: _sexOption,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _sexOption = value!;
-                              });
-                            },
+                          SizedBox(height: 20),
+                          TextFormField(
+                            controller: _fullNameController,
+                            decoration: InputDecoration(
+                              labelText: 'Full Name',
+                              border: OutlineInputBorder(),
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 10),
-                        ListTile(
-                          title: const Text('Female'),
-                          leading: Radio<String>(
-                            value: 'Female',
-                            groupValue: _sexOption,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _sexOption = value!;
-                              });
-                            },
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          controller: _numberController,
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            labelText: 'Weight (LBS)',
-                            border: OutlineInputBorder(),
-                          ),
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          controller: _addressController,
-                          decoration: InputDecoration(
-                            labelText: 'Address',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          controller: _phoneController,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            labelText: 'Phone Number',
-                            border: OutlineInputBorder(),
-                          ),
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          controller: _emailController,
-                          decoration: const InputDecoration(
-                            labelText: 'Email',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'My Health:',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 10),
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: _healthItems.length,
-                          itemBuilder: (context, index) {
-                            return CheckboxListTile(
-                              title: Text(_healthItems[index]),
-                              value: _checkedItems[index],
-                              onChanged: (bool? value) {
+                          SizedBox(height: 10),
+                          ListTile(
+                            title: const Text('Male'),
+                            leading: Radio<String>(
+                              value: 'Male',
+                              groupValue: _sexOption,
+                              onChanged: (String? value) {
                                 setState(() {
-                                  _checkedItems[index] = value!;
+                                  _sexOption = value!;
                                 });
                               },
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            _toggleEditPreferences(_isVisible);
-                          },
-                          child: Column(
-                            children: [Text('Edit Information')],
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            appState.logout();
-                          },
-                          child: Row(
-                            children: [
-                              Text('Logout'),
-                              Icon(Icons.arrow_forward),
+                          SizedBox(height: 10),
+                          ListTile(
+                            title: const Text('Female'),
+                            leading: Radio<String>(
+                              value: 'Female',
+                              groupValue: _sexOption,
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _sexOption = value!;
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: _numberController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Weight (LBS)',
+                              border: OutlineInputBorder(),
+                            ),
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
                             ],
                           ),
-                        ),
-                      ],
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: _addressController,
+                            decoration: InputDecoration(
+                              labelText: 'Address',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: _phoneController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              labelText: 'Phone Number',
+                              border: OutlineInputBorder(),
+                            ),
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'My Health:',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 10),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: _healthItems.length,
+                            itemBuilder: (context, index) {
+                              return CheckboxListTile(
+                                title: Text(_healthItems[index]),
+                                value: _checkedItems[index],
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    _checkedItems[index] = value!;
+                                  });
+                                },
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              _toggleEditPreferences(_isVisible);
+                            },
+                            child: Column(
+                              children: [Text('Edit Information')],
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              appState.logout();
+                            },
+                            child: Row(
+                              children: [
+                                Text('Logout'),
+                                Icon(Icons.arrow_forward),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
